d188b4235d280327b5ce204392654d43c181a302
Daniele Benvenuti <daniele_benvenuti@hotmail.com>
2018-08-25 15:29:43 +0200

    Primo commit di Prova della modifica

---
+++ PrimaApp/PrimaApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PrimaApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a key to Exit");
+            Console.ReadKey();
+        }
+    }
+}
